f79ea9a22c5f9528f8c2d0f33778a4d0641d0294
mmustafadogann <rampageecs@hotmail.com>
2026-08-27 16:15:21 +0300

    feat: add order read endpoints

--- trendai/src/main/java/com/trendai/trendai/repository/OrderRepository.java
+++ trendai/src/main/java/com/trendai/trendai/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.trendai.trendai.repository;
 
 import com.trendai.trendai.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findAllByUserId(Long userId, Pageable pageable);
 }
